0288385440cfb0f23d787d742e95d223cdc1a499
Flenym <egoricloud1@mail.ru>
2026-09-22 17:50:50 +0300

    iOS: fix EngineIOClient unwrap syntax (exit 65)

--- ios/Yooh/Yooh/Core/Realtime/EngineIOClient.swift
+++ ios/Yooh/Yooh/Core/Realtime/EngineIOClient.swift
@@ -170,7 +170,7 @@ final class EngineIOClient {
             let upgraded = self.wsUpgraded
             self.lock.unlock()
             guard isCurrent, !isClosed, !upgraded else { return }
-            if let error as NSError?, error.code != NSURLErrorCancelled {
+            if let nsError = error as NSError?, nsError.code != NSURLErrorCancelled {
                 #if DEBUG
                 self.log.debug("poll error, re-issuing")
                 #endif
